fc7ec3cd9b6c9608b39731b68e05a440a608acc0
Mireille-T <69097387+Mireille-T@users.noreply.github.com>
2023-08-10 22:25:11 +0800

    Frontend skeleton (minimal CSS)

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,16 @@
-// Insert JavaScript for the popup
+window.addEventListener("load", event => {
+    document.getElementById("my-critters-btn").addEventListener("click", function(){
+        openTab("my-critters")
+    });
+    document.getElementById("settings-btn").addEventListener("click", function(){
+        openTab("settings")
+    });
+})
+
+
+function openTab(tabName) {
+    if (document.getElementsByClassName("open")[0].id != tabName) {
+        document.getElementsByClassName("open")[0].classList.toggle("open");
+        document.getElementById(tabName).classList.toggle("open");
+    }
+}
